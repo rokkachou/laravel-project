f48fb1bdd29b2aa4e4ece9534c264f43ab63d52c
CHOUNEI <n.chou@rokka-systems.com>
2015-02-10 16:48:22 +0900

    firstworkbenchexcute

---
+++ workbench/skynet/mv/src/Skynet/Mv/MvServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Skynet\Mv;
+
+use Illuminate\Support\ServiceProvider;
+
+class MvServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
